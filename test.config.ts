8f0e168add093231c58c5d6b3f297499c589ad05
Cody Stanish <112568028+StanishCode@users.noreply.github.com>
2026-02-09 16:02:45 -0500

    create intial framework structure

---
+++ test.config.ts
@@ -0,0 +1,9 @@
+export class TestConfig {
+  appUrl = "https://practicesoftwaretesting.com/";
+
+  //valid login credentials
+  email = "customer@practicesoftwaretesting.com";
+  password = "welcome01";
+
+  //TODO: product details for search scenario
+}

dedc908db4396d6466a87f4f26e56a628d2417b2
Cody Stanish <112568028+StanishCode@users.noreply.github.com>
2026-07-09 15:12:22 -0400

    remove only annotation

--- tests/Security/sqlInjectionSearch.spec.ts
+++ tests/Security/sqlInjectionSearch.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "../../fixtures";
 
-test.only("user cannot inject sql in search", async ({ homePage }) => {
+test("user cannot inject sql in search", async ({ homePage }) => {
   await homePage.goToHomePage();
   await homePage.searchProduct("' OR 1=1 --");
   await expect(homePage.getNoResultsMsg()).toBeVisible();
